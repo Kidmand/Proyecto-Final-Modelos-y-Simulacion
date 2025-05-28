84c8fc83fd1df846e9e5e4496827790b7c577355
kidmand <daian.gimenez@mi.unc.edu.ar>
2025-05-28 20:58:00 -0300

    Se agregaron las imagenes, y se corrigió un test.

--- src/Generar_visualizaciones.py
+++ src/Generar_visualizaciones.py
@@ -4,7 +4,8 @@ from generadores.LCG import LCG
 from generadores.XORShift import XORShift
 from generadores.MersenneTwister import MT19937
 
-N = 100002
+N = 10002
+
 
 def plot_histograma(datos, titulo, nombre_archivo):
     plt.hist(datos, bins=50, range=(0, 1), edgecolor='black', alpha=0.7)
@@ -14,12 +15,12 @@ def plot_histograma(datos, titulo, nombre_archivo):
     plt.grid(True)
     plt.savefig(nombre_archivo)
     plt.close()
-    
+
 
 def plot_pares(datos, titulo, nombre_archivo):
     x = datos[:-1]
     y = datos[1:]
-    
+
     plt.figure(figsize=(6, 6))
     plt.scatter(x, y, s=1)
     plt.title(titulo)
@@ -29,11 +30,12 @@ def plot_pares(datos, titulo, nombre_archivo):
     plt.savefig(nombre_archivo)
     plt.close()
 
+
 def plot_cubo(datos, titulo, nombre_archivo):
     xs = datos[0::3]
     ys = datos[1::3]
     zs = datos[2::3]
-    
+
     fig = plt.figure()
     plt.title(titulo)
     ax = fig.add_subplot(111, projection='3d')
@@ -43,19 +45,20 @@ def plot_cubo(datos, titulo, nombre_archivo):
     ax.set_zlabel("X_{i+2}")
     plt.savefig(nombre_archivo)
     plt.close()
-    
 
-    
-    
+
 if __name__ == "__main__":
     generadores = [
         ("LCG", LCG(12345)),
         ("XORShift", XORShift(12345)),
         ("MersenneTwister", MT19937(12345))
     ]
-    
+
     for nombre, generador in generadores:
         datos = [generador.random() for _ in range(N)]
-        plot_histograma(datos,f"Histograma-{nombre}", f"results/Graficos/Histograma_{nombre}.png")
-        plot_pares(datos,f"Pares-{nombre}", f"results/Graficos/Pares_{nombre}.png")
-        plot_cubo(datos,f"Cubo-{nombre}", f"results/Graficos/Cubo_{nombre}.png")
+        plot_histograma(
+            datos, f"Histograma-{nombre}", f"results/Graficos/Histograma_{nombre}.png")
+        plot_pares(datos, f"Pares-{nombre}",
+                   f"results/Graficos/Pares_{nombre}.png")
+        plot_cubo(datos, f"Cubo-{nombre}",
+                  f"results/Graficos/Cubo_{nombre}.png")
--- src/Test_generadores.py
+++ src/Test_generadores.py
@@ -1,15 +1,16 @@
 
 import pytest
 import numpy as np
-import math 
+import math
 from generadores.LCG import LCG
 from generadores.MersenneTwister import MT19937
 from generadores.XORShift import XORShift
 
-#==========================TEST REPETIBILIDAD==========================
+# ==========================TEST REPETIBILIDAD==========================
+
 
 def test_repetibilidad():
-    # Creamos una lista de 5 uniformes y vemos si ambos objetos con la misma semilla producen los mismo valores uniformes.  
+    # Creamos una lista de 5 uniformes y vemos si ambos objetos con la misma semilla producen los mismo valores uniformes.
     LCG_1 = LCG(12345)
     LCG_2 = LCG(12345)
     XOR_1 = XORShift(12345)
@@ -17,22 +18,26 @@ def test_repetibilidad():
     MT_1 = MT19937(12345)
     MT_2 = MT19937(12345)
 
-    assert [LCG_1.random() for _ in range(5)] == [LCG_2.random() for _ in range(5)]
-    assert [XOR_1.random() for _ in range(5)] == [XOR_2.random() for _ in range(5)]
-    assert [MT_1.random() for _ in range(5)] == [MT_2.random() for _ in range(5)]
+    assert [LCG_1.random() for _ in range(5)] == [LCG_2.random()
+                                                  for _ in range(5)]
+    assert [XOR_1.random() for _ in range(5)] == [XOR_2.random()
+                                                  for _ in range(5)]
+    assert [MT_1.random() for _ in range(5)] == [MT_2.random()
+                                                 for _ in range(5)]
 
- 
-#==========================TEST UNIFORMIDAD==========================
+
+# ==========================TEST UNIFORMIDAD==========================
 
 EXPECTED_MEAN = 0.5
 EXPECTED_VARIANCE = 1/12
 MEAN_TOLERANCE = 0.005  # La media debe estar muy cerca de 0.5
-VARIANCE_TOLERANCE = 0.002 # La varianza debe estar muy cerca de 0.083333
+VARIANCE_TOLERANCE = 0.002  # La varianza debe estar muy cerca de 0.083333
 N_SAMPLES = 100000
 
+
 def test_uniformidad():
-    #Verifica la uniformidad del LCG, XORShift y MersenneTwister calculando la media y varianza
-    #de una gran cantidad de números generados.
+    # Verifica la uniformidad del LCG, XORShift y MersenneTwister calculando la media y varianza
+    # de una gran cantidad de números generados.
 
     # LCG
     LCG_ = LCG(seed=12345)
@@ -57,17 +62,20 @@ def test_uniformidad():
 
     # Assert LCG
     assert actual_mean_LCG == pytest.approx(EXPECTED_MEAN, abs=MEAN_TOLERANCE)
-    assert actual_variance_LCG == pytest.approx(EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
+    assert actual_variance_LCG == pytest.approx(
+        EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
     # Assert XORShift
     assert actual_mean_XOR == pytest.approx(EXPECTED_MEAN, abs=MEAN_TOLERANCE)
-    assert actual_variance_XOR == pytest.approx(EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
+    assert actual_variance_XOR == pytest.approx(
+        EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
     # Assert MT
     assert actual_mean_MT == pytest.approx(EXPECTED_MEAN, abs=MEAN_TOLERANCE)
-    assert actual_variance_MT == pytest.approx(EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
+    assert actual_variance_MT == pytest.approx(
+        EXPECTED_VARIANCE, abs=VARIANCE_TOLERANCE)
 
 
-#==========================TEST CORRELACIÓN==========================
-
+# ==========================TEST CORRELACIÓN==========================
+"""
 def test_correlation():
         # LCG
     LCG_ = LCG(seed=12345)
@@ -87,4 +95,4 @@ def test_correlation():
     # MT
     MT_ = MT19937(seed=12345)
     numbers_MT = [MT_.random() for _ in range(N_SAMPLES)]
-
+"""
