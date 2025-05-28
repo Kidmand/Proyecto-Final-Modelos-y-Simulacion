7ec3bad2e895f7b9a8f0ee45c836f7eaf93a82b4
rxciog <rocio.guadalupe.gomez@mi.unc.edu.ar>
2025-05-28 08:45:50 -0300

    Modificar valor de retorno

--- src/generadores/XORShift.py
+++ src/generadores/XORShift.py
@@ -5,17 +5,23 @@ class XORShift:
         self.b = 21
         self.c = 20
         self.y = seed
+        self.max_val = 2**32 - 1
 
     def generate(self):
         self.y ^= (self.y << self.a) & 0xFFFFFFFF
         self.y ^= (self.y >> self.b) 
         self.y ^= (self.y << self.c) & 0xFFFFFFFF
         return self.y 
+
+
+    def random(self):
+        return self.generate() / float(self.max_val)
+
     
 def main():
-    rng = XORShift(2463534242)
+    rng = XORShift(182374)
     for _ in range(10):
-        print(rng.generate())
+        print(rng.random())
 
 if __name__ == '__main__':
     main()
